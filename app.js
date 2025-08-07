a2c145143f005a826595aeb992aa104f73051e46
JoniM <jonathanmarino98@gmail.com>
2025-08-07 10:42:29 -0300

    Implementa función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let listaDeNombres = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre == ""){
+        alert("Por favor, inserte un nombre.");
+    } else{
+        listaDeNombres.push(nombre);
+        document.getElementById("amigo").value = "";
+    }
+}
+
